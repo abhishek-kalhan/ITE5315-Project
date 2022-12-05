6799746189503c863bfabea9a75f15e8a4700ba9
Abhishek Kalhan <abhishekkalhan@Abhisheks-MacBook-Air.local>
2022-12-05 17:10:55 -0500

    Added Hbs and Authentication

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
+require("dotenv").config();
 module.exports = {
   //url: "mongodb://localhost:27017/Assignment4",
-  url: "mongodb+srv://admin:abhishek@cluster0.acam1jm.mongodb.net/sample_restaurants",
+  url: process.env.url,
 };
